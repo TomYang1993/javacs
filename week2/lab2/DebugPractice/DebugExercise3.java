c7dfe9e85256375adb8d30126256a20dc97579c0
Tom Yang <xy2286@columbia.edu>
2019-01-01 18:02:02 -0500

    sentinel and lab2

---
+++ week2/lab2/DebugPractice/DebugExercise3.java
@@ -0,0 +1,25 @@
+/**
+ * Created by jug on 1/22/18.
+ */
+public class DebugExercise3 {
+    public static long countTurnips(In in) {
+        long totalTurnips = 0;
+        while (!in.isEmpty()) {
+            String vendor = in.readString();
+            String foodType = in.readString();
+            double cost = in.readDouble();
+            int numAvailable = in.readInt();
+            if (foodType.equals("turnip")) {
+                long newTotal = totalTurnips + numAvailable;
+                totalTurnips = newTotal;
+            }
+            in.readLine();
+        }
+        return totalTurnips;
+    }
+
+    public static void main(String[] args) {
+        In in = new In("foods.csv");
+        System.out.println(countTurnips(in));
+    }
+}
