27f77959ccb09fcbd15e26c74aab0288de14196c
Tom Yang <xy2286@columbia.edu>
2019-01-24 12:13:26 -0500

    added lab3

--- week3/reading_3.1/TestSort.java
+++ week3/reading_3.1/TestSort.java
@@ -1,17 +1,19 @@
-
-
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class TestSort {
 
-
-    public static void testSort() {
+    @Test
+    public void testSort() {
         String[] input = {"i", "have", "an", "egg"};
         String[] expected = {"an", "egg", "have", "i"};
         Sort.sort(input);
-        org.junit.Assert.assertArrayEquals(expected, input);
+//        org.junit.Assert.assertArrayEquals(expected, input);
+        assertArrayEquals(expected, input);
     }
 
-    public static void testFindSmallest() {
+    @Test
+    public void testFindSmallest() {
 //        String[] input = {"i", "have", "an", "egg"};
 //        String expected = "an";
 //        String actual = Sort.findSmallest(input);
@@ -25,12 +27,7 @@ public class TestSort {
         String[] input2 = {"i", "ate", "lots", "of", "eggs"};
         int expected2 = 1;
         int actual2 = Sort.findSmallest(input2, 0);
-        org.junit.Assert.assertEquals(expected2, actual2);
-    }
-
-    public static void main(String[] args) {
-        testFindSmallest();
-        testSort();
+        assertEquals(expected2, actual2);
     }
 
 }
