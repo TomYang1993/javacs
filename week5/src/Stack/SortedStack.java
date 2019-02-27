e4183073001d469272dc790efad10e11987dcb75
Tom Yang <xy2286@columbia.edu>
2019-02-26 20:33:44 -0500

    sorted stack

--- week5/src/Stack/SortedStack.java
+++ week5/src/Stack/SortedStack.java
@@ -1,6 +1,29 @@
 package Stack;
 
-public class SortedStack {
+import java.util.Stack;
 
-    
+public class SortedStack<E extends Comparable<E>> {
+
+    private Stack<E> stack;
+    private Stack<E> tempStack;
+
+    public SortedStack() {
+        stack = new Stack<E>();
+        tempStack = new Stack<E>();
+    }
+
+    public void push(E item) {
+        while(!stack.empty() && stack.peek().compareTo(item) < 0) {
+            tempStack.push(stack.pop());
+        }
+        stack.push(item);
+        while(!tempStack.empty()){
+            stack.push(tempStack.pop());
+        }
+
+    }
+
+    public E poll() {
+        return stack.pop();
+    }
 }
